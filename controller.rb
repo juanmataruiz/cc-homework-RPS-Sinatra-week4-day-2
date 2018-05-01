81a97a9cbd7602f42296c2f0beaae07cfa299d26
Juan <juanmataruiz84@gmail.com>
2018-05-01 17:03:40 +0100

    css commit

--- controller.rb
+++ controller.rb
@@ -5,5 +5,14 @@ require_relative('./models/rps')
 
 get '/game/:hand1/:hand2' do
   game = Game.new( params[:hand1], params[:hand2] )
-  return game.play
+  @outcome = game.play()
+  erb( :result )
+end
+
+get '/' do
+  erb( :home )
+end
+
+get '/game' do
+  erb( :game )
 end
